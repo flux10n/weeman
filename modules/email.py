58085dd4df080a5467079aa6a77e2378f7996b09
_ΣXPLΩ17_ <98892565+xjusthaxor@users.noreply.github.com>
2022-03-09 07:07:16 +0700

    Create email.py

---
+++ modules/email.py
@@ -0,0 +1,18 @@
+
+import smtplib
+
+# Module global configs
+MODULE_AUTHOR = "Hypsurus <hypsurus@mail.ru>"
+MODULE_LICENSE = "GPLv3"
+MODULE_VERSION = "0.1"
+MODULE_DATE = "22-01-2016"
+# Keep it short
+MODULE_DE   = "Send an email with HTML content."
+
+class Email(object):
+    """ Send HTML email """
+    def __init__(self):
+        pass
+
+def main():
+    pass
